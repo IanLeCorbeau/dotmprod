32eba71cbc3e61baa01fca3bb06d73f9f342d896
Ian LeCorbeau <I-LeCorbeau@protonmail.com>
2025-12-23 12:06:01 -0500

    complete dotfiles upload

--- home/.local/bin/net.sh
+++ home/.local/bin/net.sh
@@ -2,10 +2,8 @@
 
 CONNTYPE=$(ip route | awk '/default/ { print substr($5,1,1) }')
 if [ -z "$CONNTYPE" ]; then
-	echo "<icon>network-offline</icon>"
-	echo "<txt> down  </txt>"
+	echo "<icon>network-offline</icon><iconclick>netcon -u</iconclick>"
 elif [ "$CONNTYPE" = "e" ]; then
-	echo "<icon>network-transmit-receive</icon>"
-	echo "<txt> up  </txt>"
+	echo "<icon>network-transmit-receive</icon><iconclick>netcon -d</iconclick>"
 fi
 exit 0
